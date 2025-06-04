d963ecf2af2d118399260a5472884028c52de952
shrikant-Naik-02 <shrikantpramodnaik@example.com>
2025-06-04 16:41:16 +0530

    Add New Column Called downloadCount

--- src/main/java/com/excelfore/aws/awstask/model/File.java
+++ src/main/java/com/excelfore/aws/awstask/model/File.java
@@ -1,8 +1,10 @@
 package com.excelfore.aws.awstask.model;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.NotEmpty;
@@ -14,6 +16,8 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "files")
 public class File {
 
     @Id
@@ -30,6 +34,7 @@ public class File {
     @NotBlank
     private String awsFileName;
 
+    private long downloadCount;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
