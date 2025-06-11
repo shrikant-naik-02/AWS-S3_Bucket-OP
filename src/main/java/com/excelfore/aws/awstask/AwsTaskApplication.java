066041f6d7c46e569bfa3d254ea83e8adcf34d6e
shrikant-Naik-02 <shrikantpramodnaik@example.com>
2025-06-11 13:01:11 +0530

    Fix Pom And Main Function

--- src/main/java/com/excelfore/aws/awstask/AwsTaskApplication.java
+++ src/main/java/com/excelfore/aws/awstask/AwsTaskApplication.java
@@ -1,5 +1,7 @@
 package com.excelfore.aws.awstask;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +11,12 @@ import org.springframework.boot.context.logging.LoggingApplicationListener;
 @Slf4j
 public class AwsTaskApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(AwsTaskApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(AwsTaskApplication.class, args);
-		log.info("Hi There, Its Working");
+		log.info("Working");
+		System.out.println("Go For It");
 	}
 
 }
